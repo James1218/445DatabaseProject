af214be4fb512e397a9d4534ca1ec9861e7e310a
James1218 <jamesyang1218@gmail.com>
2016-04-18 13:55:12 -0700

    first commit

---
+++ FragmentsLab/app/src/main/java/layout/StudentFragment.java
@@ -0,0 +1,32 @@
+package layout;
+
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import fyang88.tacoma.uw.edu.fragmentslab.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ *
+ */
+public class StudentFragment extends Fragment {
+
+
+    public StudentFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_student, container, false);
+    }
+
+}
